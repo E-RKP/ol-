8f6a97c30e7a3530228a117cfc1a843733a67969
E-RKP <edobackup111@gmail.com>
2025-05-07 15:20:25 +0200

    version 1

---
+++ data.js
@@ -0,0 +1,16 @@
+let ciao = 'ciao'
+console.log(ciao)
+
+let p = ':p'
+console.log(p)
+
+
+let number = [1,2,3,4,5,6,7,8,9,10]
+let somma = 0
+
+for (i=0; i<number.length; i++){
+
+  somma = somma+number[i]
+}
+
+console.log(somma)
